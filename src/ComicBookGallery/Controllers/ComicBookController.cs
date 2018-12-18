ef2d1731e38384e10ffd3d831eb36b3739b5d52a
Chris Troy <ctcoding0627@gmail.com>
2018-12-17 16:46:00 -0800

    Added Detail action method

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -8,6 +8,10 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBookController : Controller
     {
-        
+        //action methods must be public
+        public string Detail()
+        {
+            return "Hi";
+        }
     }
 }
